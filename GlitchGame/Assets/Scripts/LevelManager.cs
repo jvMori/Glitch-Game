fd42d6b5714c3ba2b486e5bf08a816059e9d69d5
Mori <iwona.moryc92@gmail.com>
2017-07-01 20:36:59 +0200

    Splash Screen And Start Menu

---
+++ GlitchGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevel;
+    
+
+    private void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevel);
+    }
+
+    public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+        SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+}
---
+++ GlitchGame/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+
+    public AudioClip[] levelMusicChangeArray;
+    private AudioClip currentLevelMusic;
+    private AudioSource audioSource;
+
+    private LevelManager levelManager;
+    
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += this.OnLoadCallback;
+
+    }
+    // Use this for initialization
+    void Start () {
+
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+    
+    void OnLoadCallback(Scene scene, LoadSceneMode sceneMode)
+    {
+        int currIndx = SceneManager.GetActiveScene().buildIndex;
+        currentLevelMusic = levelMusicChangeArray[currIndx];
+
+        if (currentLevelMusic)
+        {
+            audioSource.clip = currentLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+   
+}
